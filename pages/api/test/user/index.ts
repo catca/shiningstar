ff743ce39105a98508529c2374ddc07344cf66d4
Hayoung <gkduddy0814@gmail.com>
2021-10-30 22:23:31 +0900

    feat : add get followList and profile apis

--- pages/api/test/user/index.ts
+++ pages/api/test/user/index.ts
@@ -3,14 +3,14 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import User from 'lib/mongoDB/models/User';
 
 export default async function handler(
-	req: NextApiRequest,
-	res: NextApiResponse,
+  req: NextApiRequest,
+  res: NextApiResponse,
 ) {
-	console.log('connect?');
-	await dbConnect();
-	console.log('connect!');
+  console.log('connect?');
+  await dbConnect();
+  console.log('connect!');
 
-	await User.find({}).then((datas: any) => {
-		res.status(200).json(datas);
-	});
+  await User.find({}).then((datas: any) => {
+    res.status(200).json(datas);
+  });
 }
