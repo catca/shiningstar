2750887d985b08e10992b3176cd47daa2e7247f5
donggune kim <kdg96g@gmail.com>
2022-01-25 01:39:27 +0900

    feat: navbar media css add
    740px보다 작으면 searchBox display: none, searchIcon display: block
    searchIcon 누르면 searchBox 볼 수 있음

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -13,6 +13,8 @@ import {
   ExploreIcon,
   FavoriteIcon,
   NewPostIcon,
+  SearchIcon,
+  BackIcon,
 } from 'components/ui/Icon';
 import SelectBox from './SelectBox';
 import { useRouter } from 'next/router';
@@ -24,6 +26,7 @@ const Navbar = () => {
   const router = useRouter();
   const [offModal, setOffModal] = useState<boolean>(false);
   const [onSelectBox, setOnSelectBox] = useState<boolean>(false);
+  const [onSearch, setOnSearch] = useState<boolean>(false)
   const selectBoxRef = useRef<HTMLDivElement>(null);
   const inputRef = useRef<any>();
   const spanRef = useRef<any>();
@@ -56,78 +59,100 @@ const Navbar = () => {
     <>
       <div className={s.paper}>
         <div className={s.navigator}>
-          <Link href="/">
-            <a className={s.mainlogo}>
-              <Image
-                src={'/instagram.png'}
-                width={'103px'}
-                height={'29px'}
-                alt={'mainlogo'}></Image>
-            </a>
-          </Link>
-          <SearchBox />
-          <div className={s.right}>
-            <div className={s.rightBanner}>
-              <Link href="/">
-                <a>
-                  <HomeIcon
-                    on={router.asPath === '/' && !onSelectBox}
-                  />
-                </a>
-              </Link>
-              {/* <Link href="/direct">
-                <a>
-                  <DirectIcon />
-                </a>
-              </Link> */}
-
-              <Link href="/explore">
-                <a>
-                  <ExploreIcon
-                    on={router.asPath === '/explore' && !onSelectBox}
-                  />
-                </a>
-              </Link>
-
-              {/* <div>
-                <FavoriteIcon />
-              </div> */}
-
-              <div onClick={() => dispatch(setModal('newPost', true))}>
-                <div>
-                  <NewPostIcon />
+          {
+            onSearch ?
+              <>
+                <div onClick={() => setOnSearch(false)}>
+                  <BackIcon />
                 </div>
-              </div>
+                <SearchBox media={true} />
+                <div style={{ width: '24px' }}></div>
+              </>
+              :
+              <>
+                <Link href="/">
+                  <a className={s.mainlogo}>
+                    <Image
+                      src={'/instagram.png'}
+                      width={'103px'}
+                      height={'29px'}
+                      alt={'mainlogo'}></Image>
+                  </a>
+                </Link>
+                <SearchBox media={false} />
+                <div className={s.right}>
+                  <div className={s.rightBanner}>
+                    <Link href="/">
+                      <a>
+                        <HomeIcon
+                          on={router.asPath === '/' && !onSelectBox}
+                        />
+                      </a>
+                    </Link>
 
-              {/* TODO:누르면 메뉴바 나오도록 */}
-              <div>
-                <span
-                  ref={spanRef}
-                  onClick={() => {
-                    setOnSelectBox(true);
-                  }}
-                  style={{ display: 'flex', transform: 'translateY(-2px)' }}>
-                  <ProfileImage
-                    size={'nav'}
-                    border={
-                      router.asPath !== '/'
-                      && router.asPath !== '/explore'
-                      || onSelectBox
-                    }
-                    borderColor={'black'}
-                    imageUrl={userInfo.profileImageUrl}
-                  />
-                </span>
-                {onSelectBox && (
-                  <div ref={selectBoxRef}>
-                    <SelectBox
-                      closeModal={() => setOffModal(true)}
-                    />
+                    <div
+                      className={s.searchIconWrapper}
+                      onClick={() => setOnSearch(true)}>
+                      <div>
+                        <SearchIcon navbar={true} />
+                      </div>
+                    </div>
+
+                    {/* <Link href="/direct">
+                      <a>
+                        <DirectIcon />
+                      </a>
+                    </Link> */}
+
+                    <Link href="/explore">
+                      <a>
+                        <ExploreIcon
+                          on={router.asPath === '/explore' && !onSelectBox}
+                        />
+                      </a>
+                    </Link>
+
+                    {/* <div>
+                      <FavoriteIcon />
+                    </div> */}
+
+                    <div onClick={() => dispatch(setModal('newPost', true))}>
+                      <div>
+                        <NewPostIcon />
+                      </div>
+                    </div>
+
+                    {/* TODO:누르면 메뉴바 나오도록 */}
+                    <div>
+                      <span
+                        ref={spanRef}
+                        onClick={() => {
+                          setOnSelectBox(true);
+                        }}
+                        style={{ display: 'flex', transform: 'translateY(-2px)' }}>
+                        <ProfileImage
+                          size={'nav'}
+                          border={
+                            router.asPath !== '/'
+                            && router.asPath !== '/explore'
+                            || onSelectBox
+                          }
+                          borderColor={'black'}
+                          imageUrl={userInfo.profileImageUrl}
+                        />
+                      </span>
+                      {onSelectBox && (
+                        <div ref={selectBoxRef}>
+                          <SelectBox
+                            closeModal={() => setOffModal(true)}
+                          />
+                        </div>
+                      )}
+                    </div>
                   </div>
-                )}
-              </div>
-            </div>
-          </div>
+                </div>
+              </>
+          }
         </div>
       </div>
     </>
